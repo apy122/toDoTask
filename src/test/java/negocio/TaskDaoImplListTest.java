a250b0114d11a9b80dd20c319a43e6bf4f582de2
Alvaro <alvaro.perez532@comunidadunir.net>
2025-02-26 19:21:30 +0100

    Add: create TaskDaoImplListTest.java and UsuarioDaoimpListTest.java

---
+++ src/test/java/negocio/TaskDaoImplListTest.java
@@ -0,0 +1,32 @@
+package negocio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskDaoImplListTest {
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void insertOne() {
+    }
+
+    @Test
+    void updateOne() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void deleteByObjeto() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
---
+++ src/test/java/negocio/UsuarioDaoimpListTest.java
@@ -0,0 +1,32 @@
+package negocio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioDaoimpListTest {
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void insertOne() {
+    }
+
+    @Test
+    void updateOne() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void deleteByObjeto() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
